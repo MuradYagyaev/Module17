f6800e1a0e0089f258b548bc4c2545e3d87be9e7
MuradYagyaev <165575932+MuradYagyaev@users.noreply.github.com>
2024-10-01 18:08:22 +0300

    Add files via upload

---
+++ app_v2/models/task.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import CreateTable
+
+from app_v2.backend.db import Base
+from app_v2.models import *
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+    __table_args__ = {'keep_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('users.id'), index=True, nullable=False)
+    slug = Column(String, unique=True, index=True)
+    user = relationship('User', back_populates='tasks')
+
+
+print(CreateTable(Task.__table__))
